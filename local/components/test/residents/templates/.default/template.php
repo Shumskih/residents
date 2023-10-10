dc168427b7597b1a3b01be80f2cfafb6f0f55670
aleksander <aleksandr.shumeiko@simbirsoft.com>
2023-10-10 16:24:51 +0300

    удалил лишние комментарии

--- local/components/test/residents/templates/.default/template.php
+++ local/components/test/residents/templates/.default/template.php
@@ -4,19 +4,7 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
     die();
 }
 
-/**
- * @var array $arParams
- * @var array $arResult
- * @global CMain $APPLICATION
- * @global CUser $USER
- * @global CDatabase $DB
- * @var CBitrixComponentTemplate $this
- * @var string $templateName
- * @var string $templateFile
- * @var string $templateFolder
- * @var string $componentPath
- * @var CBitrixComponent $component
- */
+/** @var array $arResult */
 
 $this->setFrameMode(true); ?>
 <h1>Тест</h1>
